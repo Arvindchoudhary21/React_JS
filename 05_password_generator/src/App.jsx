18ce0f12883783bc71b90ecc163bfdc85acbce17
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-24 22:35:49 +0530

    d

--- 05_password_generator/src/App.jsx
+++ 05_password_generator/src/App.jsx
@@ -78,7 +78,7 @@ function App() {
 
         <div className=' flex text-sm gap-x-2'>
           <div className=' flex items-center gap-x-1'>
-            <input type="range" min={6} max={100} value={length} className=' cursor-pointer'
+            <input type="range" min={8} max={100} value={length} className=' cursor-pointer'
               onChange={(e) => { setLength(e.target.value) }}
             />
             <label> Length : {length}</label>
